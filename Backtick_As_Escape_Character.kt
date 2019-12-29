9d0cbef278d0f26c96d3700f36df7933aa470cab
Dheeraj357 <54169677+Dheeraj357@users.noreply.github.com>
2019-12-29 14:18:49 +0530

    This is use of Special Character in Kotlin ...

---
+++ Backtick_As_Escape_Character.kt
@@ -0,0 +1,6 @@
+﻿// Write A Program  Explaning Concept OF Backtick[`] As Escape Character ...
+// ` is used for only specifing that method which is named as  keyword name in kotlin like
+fun `in`()
+{
+ println("This is Use Of Backtick ...")
+}  
---
+++ Companion Object .kt
@@ -0,0 +1,51 @@
+﻿//Write A Program For Explaning Concept Of Companion Object ...
+// java and kotlin inter-operable...
+class A
+{
+ companion object
+{
+//this anotation is limites with Companion Object  only...
+@JvmStatic //anotation in kotlin used for java file ...  
+   fun show()
+{
+  println("Hello Friends ....!!")
+}
+}  
+}
+fun main(args : Array<String>)
+{
+A.show()
+}
+	file2.java
+public class Runner
+{
+public static void main(String[] a)
+{
+A.show()
+}
+}
+NOTE-::
+// If You Want To Run Above File On Kotlin plateform Than remove anotation ...
+OUTPUT--->>>
+//This is Same For Both..
+Hello Friends ....!!
+###########################################
+//Write A Program For Factory Pattern By Companion object ...
+class A
+{
+//Here Companion object is usefull for creation complex logic of object creation   
+companion object{
+   fun create(): A=A()    
+   }
+fun show()
+{
+println("in Show ...")
+}
+}
+fun main(args:Array<String>)
+{
+var obj=A.create()
+obj.show()
+}
+OUTPUT--->>>
+in Show ...
